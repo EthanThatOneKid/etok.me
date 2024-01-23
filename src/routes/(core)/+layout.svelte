ed9edf7aad70acca0022abdb0230f00a19c24914
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-01-22 23:51:11 -0800

    add layout file

---
+++ src/routes/(core)/+layout.svelte
@@ -0,0 +1,9 @@
+<style>
+	:global(body) {
+		display: grid;
+		grid: [stack] 1fr / min-content [stack] 1fr;
+	}
+
+	/* https://youtu.be/uiZqDLqjGRY?t=187 */
+	/* https://youtu.be/oHcTn83M1ls */
+</style>
