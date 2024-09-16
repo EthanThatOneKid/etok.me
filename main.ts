dfa17be055727af8e80df5f1bfb9f7732323f8fd
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-09-16 16:09:06 -0700

    implement `go.fart.tools`

--- main.ts
+++ main.ts
@@ -1,19 +1,42 @@
 import type { Route } from "@std/http/unstable-route";
 import { route } from "@std/http/unstable-route";
+import { go } from "go.fart.tools";
 
-const GITHUB_URL = new URL("https://github.com/EthanThatOneKid");
+export default {
+  fetch(request) {
+    return router(request);
+  },
+} satisfies Deno.ServeDefaultExport;
 
-const routes: Route[] = [
+export const routes: Route[] = [
   {
-    pattern: new URLPattern({ pathname: "/" }),
-    handler: () => Response.redirect(GITHUB_URL),
+    method: "GET",
+    pattern: new URLPattern({ pathname: "/:path*" }),
+    handler(request, _info) {
+      const url = new URL(request.url);
+      const destinationURL = go(
+        url,
+        {
+          "and": "https://and.etok.me",
+          "codes": "https://etok.codes",
+          "id": "https://id.etok.me",
+        },
+      );
+      return new Response(
+        null,
+        {
+          status: 301,
+          headers: {
+            "Location": destinationURL.toString(),
+          },
+        },
+      );
+    },
   },
 ];
 
-function defaultHandler(_request: Request) {
+export function defaultHandler(_request: Request) {
   return new Response("Not found", { status: 404 });
 }
 
-if (import.meta.main) {
-  Deno.serve(route(routes, defaultHandler));
-}
+export const router = route(routes, defaultHandler);
