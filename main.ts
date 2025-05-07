14e24155c71e54a4b35f36df3d8866ad3570fcdc
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-05-06 22:34:27 -0700

    Migrate to GitHub Pages

--- main.ts
+++ main.ts
@@ -1,53 +1,25 @@
-import type { Route } from "@std/http/unstable-route";
-import { route } from "@std/http/unstable-route";
-import { serveDir } from "@std/http/file-server";
 import { parse } from "@std/jsonc";
 import { go } from "go.fart.tools";
-import { useGoogleAnalytics } from "./google-analytics.ts";
 
 if (import.meta.main) {
-  Deno.serve(
-    useGoogleAnalytics(
-      Deno.env.get("GOOGLE_ANALYTICS_ID"),
-      (request) => router(request),
-    ),
-  );
+  const output = Deno.args[0] ?? "./static";
+  await Deno.mkdir(output, { recursive: true });
+  await Deno.copyFile("./index.html", `${output}/index.html`);
+
+  const shortlinks = parse(
+    await Deno.readTextFile(new URL(import.meta.resolve("./shortlinks.jsonc")))
+  ) as Record<string, string>;
+  for (const alias of Object.keys(shortlinks)) {
+    const url = go(new URL(`https://etok.me/${alias}`), shortlinks);
+    await Deno.mkdir(`${output}/${alias}`, { recursive: true });
+    await Deno.writeTextFile(
+      `${output}/${alias}/index.html`,
+      `<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url=${url}" />
+  </head>
+</html>`
+    );
+  }
 }
-
-export const routes: Route[] = [
-  {
-    method: "GET",
-    pattern: new URLPattern({ pathname: "/:path*" }),
-    handler(request) {
-      const destinationURL = go(new URL(request.url), shortlinks);
-      return new Response(null, {
-        status: 301,
-        headers: {
-          Location: destinationURL.toString(),
-        },
-      });
-    },
-  },
-  {
-    method: "GET",
-    pattern: new URLPattern({ pathname: "/*" }),
-    handler(request) {
-      return serveDir(request.clone(), {
-        fsRoot: "./static",
-        showIndex: true,
-      });
-    },
-  },
-];
-
-export function defaultHandler(_request: Request) {
-  return new Response("Not found", { status: 404 });
-}
-
-export const router = route(routes, defaultHandler);
-
-export const shortlinks = parse(
-  await Deno.readTextFile(
-    new URL("./static/shortlinks.jsonc", import.meta.url),
-  ),
-) as Record<string, string>;
